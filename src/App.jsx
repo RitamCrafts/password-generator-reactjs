b0f12c1825b3c79ab8035855b44b35d7b9a42837
RitamCrafts <ritamrm1157@gmail.com>
2026-05-05 14:33:48 +0530

    full-commit

---
+++ src/App.jsx
@@ -0,0 +1,90 @@
+import { useState, useCallback, useEffect, useRef } from "react";
+import React from 'react'
+
+function App() {
+  const [length,setlength]=useState(8);
+  const [numberAllowed,setNumberAllowed]=useState(true);
+  const [specialAllowed,setSpecialAllowed]=useState(false);
+  const [password,setPassword]=useState("");
+
+  const passwordRef=useRef(null);
+  
+  const passwordGenerator = useCallback(()=>{
+    let pass="";
+    let str="ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+    if (numberAllowed) str+="0123456789";
+    if (specialAllowed) str+="-_.@#$%^&*!,+-";
+
+    for (let i = 1; i <= length; i++) {
+      let chIndex=Math.floor(Math.random()*str.length+1);
+      pass=pass+str.charAt(chIndex);
+    }
+
+    setPassword(pass);
+
+  },[length,numberAllowed,specialAllowed,setPassword] )
+
+  useEffect(()=>{passwordGenerator()},[passwordGenerator])
+
+  const copyPasswordToClipboard=()=>{
+    passwordRef.current?.select();
+    passwordRef.current?.setSelectionRange(0,length);
+    window.navigator.clipboard.writeText(password);
+  }
+
+  return (
+    <div className="h-screen w-screen bg-slate-950 p-2">
+      
+      <div className=" bg-gray-900 container rounded-lg w-full max-w-md mx-auto mt-1 py-6 px-4 text-orange-400 shadow-md">
+        <p className="text-center mb-2">Password Generator</p>
+        <div className="searchAndCopy flex rounded-lg shadow mb-4 overflow-hidden h-10">
+          <input 
+          type="text"
+          value={password}
+          className="py-1 px-3 w-full outline-none bg-gray-800"
+          placeholder="password"
+          ref={passwordRef}
+          readOnly
+          />
+          <button onClick={()=>copyPasswordToClipboard()} className="text-white bg-orange-400 outline-none px-3 cursor-pointer hover:opacity-80 active:opacity-55">Copy</button>
+        </div>
+
+        <div className="line2 flex justify-between">
+          <div className="left-side flex gap-1 mr-1">
+            <input 
+            type="range" 
+            className="cursor-pointer accent-orange-500"
+            value={length}
+            max={30}
+            min={3}
+            onChange={(e)=>setlength(e.target.value)}
+            />
+            <label>Length: {length}</label>
+          </div>
+          <div className="right-side flex gap-1 ml-1">
+            <input 
+            type="checkbox"
+            checked={numberAllowed}
+            className="accent-amber-500"
+            onChange={()=>setNumberAllowed((prev)=>!prev)}
+            />
+            <label className="mr-1">Numbers</label>
+
+            <input 
+            type="checkbox"
+            checked={specialAllowed}
+            className="accent-amber-500"
+            onChange={()=>setSpecialAllowed((prev)=>!prev)}
+            />
+            <label className="mr-1">Special</label>
+          </div>
+
+
+        </div>
+      </div>
+      
+    </div>
+  )
+}
+
+export default App
